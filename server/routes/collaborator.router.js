9bbacc93f1461232922f8230847e8bcc5e965821
abgeiger <30698092+abgeiger@users.noreply.github.com>
2018-01-26 13:48:15 -0600

    add collaborator router

---
+++ server/routes/collaborator.router.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const pool = require('../modules/pool.js');
+const path = require('path');
+
+// Main collaborator get
+router.get('/', function (req, res) {
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('error', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query(`PUT SQL HERE`, function (errorMakingDatabaseQuery, result) {
+                done();
+                if (errorMakingDatabaseQuery) {
+                    console.log('error', errorMakingDatabaseQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
+}); // end collaborator get
+
+// Main collaborator post
+router.post('/', function (req, res) {
+
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('error', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query(`PUT SQL HERE`, [], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    res.sendStatus(500)
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        }
+    });
+}); // end collaborator post
+
+// Main collaborator put
+router.put('/', function (req, res) {
+
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('error', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query(`PUT SQL HERE`, [], function (errorMakingDatabaseQuery, result) {
+                done();
+                if (errorMakingDatabaseQuery) {
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        }
+    });
+}); // end collaborator put
+
+// Main collaborator delete
+router.delete('/', function (req, res) {
+
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('error', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query(`PUT SQL HERE`, [], function (errorMakingDatabaseQuery, result) {
+                done();
+                if (errorMakingDatabaseQuery) {
+                    console.log('error', errorMakingDatabaseQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        }
+    });
+}); // end collaborator delete
+
+// Collaborator search get
+router.get('/search', function (req, res) {
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('error', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query(`PUT SQL HERE`, function (errorMakingDatabaseQuery, result) {
+                done();
+                if (errorMakingDatabaseQuery) {
+                    console.log('error', errorMakingDatabaseQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
+}); // end collaborator search get
+
+module.exports = router;
