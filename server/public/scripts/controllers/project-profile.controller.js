4ea739f3f28c6d8cb36d91e5a24cf7bb7584b11f
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2018-01-28 16:27:35 -0600

    Move message modal to the service

--- server/public/scripts/controllers/project-profile.controller.js
+++ server/public/scripts/controllers/project-profile.controller.js
@@ -8,35 +8,13 @@ myApp.controller('ProjectProfileController', function(UserService, ProjectServic
 
     self.projectArray = ProjectService.projectArray
 
-    // self.contactProjectOwner = ProjectService.contactProjectOwner
-    self.contactProjectOwner = function(ev) {
-        console.log('button Clicked');
-        $mdDialog.show({
-          controller: 'ProjectProfileController as vm',
-          templateUrl: '../views/modals/loginregister.dialog.html',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          clickOutsideToClose:true,
-          fullscreen: self.customFullscreen 
-        })
-        
+    self.contactProjectOwner = ProjectService.contactProjectOwner
+
+
+      self.sendMessage = function(message){
+          console.log('message: ', message);
+          
       }
-      self.showPrompt = function(ev) {
-        // Appending dialog to document.body to cover sidenav in docs app
-        $mdDialog.prompt()
-          .title('What would you name your dog?')
-          .textContent('Bowser is a common name.')
-          .placeholder('Dog name')
-          .initialValue('Buddy')
-          .targetEvent(ev)
-          .required(true)
-          .ok('Okay!')
-          .cancel('I\'m a cat person');
-    
-        $mdDialog.show(confirm).then(function(result) {
-          $scope.status = 'You decided to name your dog ' + result + '.';
-        }, function() {
-          $scope.status = 'You didn\'t name your dog.';
-        });
-      };
+
+
 });
--- server/public/scripts/services/project.service.js
+++ server/public/scripts/services/project.service.js
@@ -1,22 +1,21 @@
-myApp.service('ProjectService', function ($http, $location) {
+myApp.service('ProjectService', function ($http, $location, $mdDialog) {
     console.log('ProjectService Loaded');
     const self = this;
 
     self.projectArray = { list: [] };
 
-
-    // self.contactProjectOwner = function(ev) {
-    //   console.log('button Clicked');
-    //   $mdDialog.show({
-    //     controller: 'LoginController as lc',
-    //     templateUrl: '../views/modals/loginregister.dialog.html',
-    //     parent: angular.element(document.body),
-    //     targetEvent: ev,
-    //     clickOutsideToClose:true,
-    //     fullscreen: self.customFullscreen 
-    //   })
-      
-    // }
+    //Modal for sending a message to project owners
+    self.contactProjectOwner = function(ev) {
+      console.log('button Clicked');
+      $mdDialog.show({
+        controller: 'ProjectProfileController as vm',
+        templateUrl: '../views/modals/contactprojectowner.dialog.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: self.customFullscreen 
+      })     
+    };
 
     //Get all projects
     self.getProjects = function () {
