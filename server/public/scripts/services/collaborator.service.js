b49831492fb98532622b4a76e7c41632b7686a62
abgeiger <30698092+abgeiger@users.noreply.github.com>
2018-01-30 09:52:57 -0600

    remove sample data from collaborator service

--- server/public/scripts/services/collaborator.service.js
+++ server/public/scripts/services/collaborator.service.js
@@ -2,12 +2,6 @@ myApp.service('CollaboratorService', function ($http, $location) {
     console.log('CollaboratorService Loaded');
     const self = this;
 
-    const examplePeople = [
-      {username: 'Top-hat Jones', user_picture: '../views/images/profile-default.jpg', skill_name: 'Hiding'},
-      {username: 'Mr. Poopybutthole', user_picture: '../views/images/profile-default.jpg', skill_name: 'Talking'},
-      {username: 'Mr. Meseeks', user_picture: '../views/images/profile-default.jpg', skill_name: 'Helping'}
-    ];
-
     self.collaborators = { list: [] };
 
     // Get all collaborator data for collaborator search view
