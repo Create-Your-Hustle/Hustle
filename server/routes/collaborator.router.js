d10a50f33870a50249d44b087a7b97e6b7eb1585
IcusFMagni <danika.ragnhild@gmail.com>
2018-01-30 15:25:07 -0600

    get even more data on the user page

--- server/routes/collaborator.router.js
+++ server/routes/collaborator.router.js
@@ -13,7 +13,8 @@ router.get('/select', function (req, res) {
             res.sendStatus(500);
         } else {
             // this query needs to be changed to include concatonated skills
-            client.query(`SELECT users.username, users.user_picture, skills.skill_name, users_skills.skill_rating FROM users
+            client.query(`SELECT users.username, users.user_picture, skills.skill_name, users_skills.skill_rating, users.user_city, users.user_state, users.user_remote,
+                users.user_for_pay, users.user_for_trade, users.user_bio FROM users
                 LEFT JOIN users_skills ON users.id=users_skills.user_id
                 LEFT JOIN skills ON users_skills.skill_id=skills.skill_id
                 WHERE users.username = $1;`,[req.query.name], function (errorMakingDatabaseQuery, result) {
