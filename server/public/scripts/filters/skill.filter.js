bfad13d3bc7a7f80e882a038ad3ec628af7f4c2b
Paige Scheunemann <pls927@gmail.com>
2018-01-31 09:24:53 -0600

    start custom search filter

--- server/public/scripts/filters/skill.filter.js
+++ server/public/scripts/filters/skill.filter.js
@@ -1,17 +1,27 @@
 myApp.filter('skill', function(){
-    return function (array, skills) {
-        var skillList = {
+    return function (resultArray, skills) {
+        const skillList = {
             skills: skills,
             out: []
         };
-        angular.forEach(array, function (value, key) {
-            console.log(value.skill_list)
-            let check = value.skill_list;
-            if(str.match(skills))
-            // if (this.array[value.skill_list] === true) {
-            //     this.out.push(value);
-            // }
-        }, skillList);
+
+        const skillArray = resultArray[i].skill_list
+
+        for (let i = 0; i < resultArray.length; i++) {
+            for (let j = 0; j < skills.length; j++) {
+                const skill = skills[j];
+                let isMatch = skillArray.includes(skills[0])
+            }
+        }
+
+        // angular.forEach(array, function (value, key) {
+        //     console.log(value.skill_list)
+        //     let check = value.skill_list;
+        // //     if(str.match(skills)
+        // //     // if (this.array[value.skill_list] === true) {
+        // //     //     this.out.push(value);
+        // //     // }
+        // // }, skillList);
         return skillList.out;
     };
 });
