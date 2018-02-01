972ebca00d2d256848c0a0a80cd2b8bd24c6e04b
Paige Scheunemann <pls927@gmail.com>
2018-02-01 09:10:47 -0600

    search project view

--- server/public/scripts/controllers/project-search.controller.js
+++ server/public/scripts/controllers/project-search.controller.js
@@ -17,5 +17,46 @@ myApp.controller('ProjectSearchController', function(UserService, ProjectService
         console.log('EVENTS', self.filteredProjects);
     }
 
+    function debounce(func, wait, context) {
+        var timer;
+
+        return function debounced() {
+            var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+            $timeout.cancel(timer);
+            timer = $timeout(function () {
+                timer = undefined;
+                func.apply(context, args);
+            }, wait || 10);
+        };
+    }
+
+    self.toggleLeft = buildDelayedToggler('left');
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+        return debounce(function () {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+                .toggle()
+                .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                });
+        }, 200);
+    }
+
+    function buildToggler(navID) {
+        return function () {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+                .toggle()
+                .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                });
+        };
+    }
 
 });
