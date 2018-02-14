5df46da0c5ac1a932b9fd2d88d24646afba7c842
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2018-02-14 10:09:50 -0600

    updated database

--- database.sql
+++ database.sql
@@ -1,84 +1,89 @@
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	username VARCHAR(50),
-	password VARCHAR(100),
-	display_name VARCHAR(50) UNIQUE,
-	user_bio VARCHAR(1000),
-	user_rating DECIMAL,
-	user_picture VARCHAR(150),
-	-- Changed character count to 150, LinkedIn picture I tried to upload here failed because address was too long
-	user_place_id VARCHAR,
-	user_city VARCHAR(20),
-	user_state VARCHAR(20),
-	user_remote BOOLEAN,
-	user_for_pay BOOLEAN,
-	user_for_trade BOOLEAN,
-	user_duration_min INT,
-	user_duration_max INT,
-	user_weekly_min INT,
-	user_weekly_max INT
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE,
+    password VARCHAR(100),
+    user_bio VARCHAR(2000),
+    user_rating DECIMAL,
+    user_picture VARCHAR(150),
+    user_place_id VARCHAR,
+    user_city VARCHAR(50),
+    user_state VARCHAR(20),
+    user_remote BOOLEAN DEFAULT true,
+    user_for_pay BOOLEAN DEFAULT true,
+    user_for_trade BOOLEAN DEFAULT true,
+    user_duration_min INT,
+    user_duration_max INT,
+    user_weekly_min INT,
+    user_weekly_max INT,
+    code VARCHAR(20),
+    display_name VARCHAR(150)
 );
 
 
 
 CREATE TABLE skills (
-	skill_id SERIAL PRIMARY KEY,
-	skill_name VARCHAR(50) NOT NULL UNIQUE
+    skill_id SERIAL PRIMARY KEY,
+    skill_name VARCHAR(50) NOT NULL UNIQUE
 );
 
 
 
 CREATE TABLE projects (
-	project_id SERIAL PRIMARY KEY,
-	project_name VARCHAR(50) NOT NULL,
-	project_description VARCHAR(1000),
-	project_picture VARCHAR(100),
-	project_place_id VARCHAR(50),
-	project_city VARCHAR(20),
-	project_state VARCHAR(20),
-	project_remote BOOLEAN NOT NULL,
-	project_for_pay BOOLEAN NOT NULL,
-	project_for_trade BOOLEAN NOT NULL,
-	project_duration_min INT,
-	project_duration_max INT,
-	project_weekly_min INT,
-	project_weekly_max INT
+    project_id SERIAL PRIMARY KEY,
+    project_name VARCHAR(50) NOT NULL,
+    project_description VARCHAR(1000),
+    project_picture VARCHAR(150),
+    project_place_id VARCHAR(50),
+    project_city VARCHAR(20),
+    project_state VARCHAR(20),
+    project_remote BOOLEAN,
+    project_for_pay BOOLEAN,
+    project_for_trade BOOLEAN,
+    project_duration_min INT,
+    project_duration_max INT,
+    project_weekly_min INT,
+    project_weekly_max INT
 );
 
 
 
 CREATE TABLE users_skills (
-	user_skill_id SERIAL PRIMARY KEY,
-	skill_rating DECIMAL NOT NULL,
-	user_id INT REFERENCES users,
-	skill_id INT REFERENCES skills
+    user_skill_id SERIAL PRIMARY KEY,
+    skill_rating DECIMAL NOT NULL,
+    user_id INT REFERENCES users,
+    skill_id INT REFERENCES skills
 );
 
 
 
 CREATE TABLE projects_skills (
-	project_skill_id SERIAL PRIMARY KEY,
-	required_rating INT NOT NULL,
-	project_id INT REFERENCES projects,
-	skill_id INT REFERENCES skills
+    project_skill_id SERIAL PRIMARY KEY,
+    required_rating INT NOT NULL,
+    project_id INT REFERENCES projects,
+    skill_id INT REFERENCES skills
 );
 
 
 
 CREATE TABLE users_projects (
-	user_project_id SERIAL PRIMARY KEY,
-	can_edit BOOLEAN NOT NULL,
-	user_id INT REFERENCES users,
-	project_id INT REFERENCES projects,
-	user_project_role VARCHAR(100) NOT NULL
+    user_project_id SERIAL PRIMARY KEY,
+    can_edit BOOLEAN NOT NULL,
+    user_id INT REFERENCES users,
+    project_id INT REFERENCES projects,
+    user_project_role VARCHAR(100) NOT NULL,
+    collaboration_request BOOLEAN,
+    collaborator BOOLEAN
 );
 
 
 
 CREATE TABLE ratings (
-	rating_id SERIAL NOT NULL,
-	reviewed_user_id INT REFERENCES users,
-	reviewer_id INT REFERENCES users,
-	rating INT NOT NULL
+    rating_id SERIAL NOT NULL,
+    reviewed_user_id INT REFERENCES users,
+    reviewer_id INT REFERENCES users,
+    communication INT,
+    followthrough INT,
+    friendliness INT,
+    accuracy INT,
+    overall INT
 );
-
